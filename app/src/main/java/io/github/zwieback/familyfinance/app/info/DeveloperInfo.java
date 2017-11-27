db4b44566dc7e8e6a621b5f2c09d7e4959d1d15e
Anton Avdeev <zw13back@gmail.com>
2017-11-27 15:52:47 +0400

    changed default email

--- app/src/main/java/io/github/zwieback/familyfinance/app/info/DeveloperInfo.java
+++ app/src/main/java/io/github/zwieback/familyfinance/app/info/DeveloperInfo.java
@@ -2,7 +2,7 @@ package io.github.zwieback.familyfinance.app.info;
 
 public final class DeveloperInfo {
 
-    public static final String EMAIL = "anton.avdeev.freelancer@gmail.com";
+    public static final String EMAIL = "anton.avdeev.developer@gmail.com";
 
     private DeveloperInfo() {
     }
