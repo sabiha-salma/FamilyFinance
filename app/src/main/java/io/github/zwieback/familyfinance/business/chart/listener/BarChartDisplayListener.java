a57b807a4a414deec54a94bc23c581d384e91d4a
Anton Avdeev <zw13back@gmail.com>
2017-12-13 15:43:50 +0400

    removed unused listener

--- app/src/main/java/io/github/zwieback/familyfinance/business/chart/listener/BarChartDisplayListener.java
+++
@@ -1,6 +0,0 @@
-package io.github.zwieback.familyfinance.business.chart.listener;
-
-import io.github.zwieback.familyfinance.business.chart.display.BarChartDisplay;
-
-public interface BarChartDisplayListener extends ChartDisplayListener<BarChartDisplay> {
-}
