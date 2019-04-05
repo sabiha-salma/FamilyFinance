5f39ec593d4ddce8d5b72a51fab886905d1bff71
Anton Avdeev <zw13back@gmail.com>
2019-04-05 10:44:15 +0400

    moved database preferences to the appropriate package

--- app/src/main/java/io/github/zwieback/familyfinance/business/preference/custom/database/ArticlePreference.java
+++ app/src/main/java/io/github/zwieback/familyfinance/business/preference/custom/database/ArticlePreference.java
@@ -1,4 +1,4 @@
-package io.github.zwieback.familyfinance.business.preference.custom;
+package io.github.zwieback.familyfinance.business.preference.custom.database;
 
 import android.content.Context;
 import android.content.Intent;
